f64699ccb56d88ae731f61c4e3f952132f7b621d
Abraham Ghani <AbrahamKosmoGhani@gmail.com>
2025-06-26 09:46:53 -0700

    added autowired and class annotations to the ShoppingCartController

--- capstone-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,6 +1,8 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
@@ -8,16 +10,31 @@ import org.yearup.data.UserDao;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.User;
 
+
 import java.security.Principal;
 
-// convert this class to a REST controller
-// only logged in users should have access to these actions
+
+
+
+@RestController
+@RequestMapping("cart")
+@CrossOrigin
 public class ShoppingCartController
 {
     // a shopping cart requires
     private ShoppingCartDao shoppingCartDao;
     private UserDao userDao;
     private ProductDao productDao;
+
+    @Autowired
+    public ShoppingCartController(UserDao userDao,
+                                  ShoppingCartDao shoppingCartDao,
+                                  ProductDao productDao)
+    {
+        this.userDao = userDao;
+        this.shoppingCartDao = shoppingCartDao;
+        this.productDao = productDao;
+    }
 
 
 
